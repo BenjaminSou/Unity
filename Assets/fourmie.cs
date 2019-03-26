77e50bb1c6403cc595517e6dee1161b73f69394a
Benjamin <benjamin.soulan@depinfonancy.net>
2019-03-26 11:45:01 +0100

    Ajout test

---
+++ Assets/fourmie.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fourmie : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") != 0){ Debug.Log("Axis Val =" + Input.GetAxis("Horizontal")); }
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 1.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
